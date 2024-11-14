1a0a436d3cfe948528001365312bba456be3583f
Janbei16 <jannisbeichler@gmail.com>
2024-11-14 16:39:59 +0100

    multi jobs

--- src/app/components/Header.test.js
+++ src/app/components/Header.test.js
@@ -1,10 +1,20 @@
-import { render, screen } from '@testing-library/react';
+/* import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Header from './Header';
+
  
-test('renders links to subpages', () => {
-  render(<Header />);
-  const linkElement1 = screen.getByText(/Home/i);
-  const linkElement2 = screen.getByText(/About/i);
-  expect(linkElement1).toBeInTheDocument();
-  expect(linkElement2).toBeInTheDocument();
-});
+describe('Header component', () => {
+  test('renders header with correct links', () => {
+    render(<Header)
+    //render(<Header />);
+    // Überprüfe, ob der Link zur Startseite angezeigt wird
+    expect(screen.getByText('Einkaufsliste App')).toBeInTheDocument();
+    // Überprüfe, ob der Link "Neue Liste erstellen" angezeigt wird
+    expect(screen.getByText('Neue Liste erstellen')).toBeInTheDocument();
+    // Überprüfe, ob der Link "Liste beitreten" angezeigt wird
+    expect(screen.getByText('Liste beitreten')).toBeInTheDocument();
+ 
+    // Überprüfe, ob der Link "Einkaufsliste anzeigen" angezeigt wird
+    expect(screen.getByText('Einkaufsliste anzeigen')).toBeInTheDocument();
+  });
+}); */
